32066f984187afbf09ab6b168d76cc05a376a440
Alan Gomez <agomezh1901@alumno.ipn.mx>
2024-10-12 20:17:49 -0600

    backend

---
+++ backend/prosperify/src/main/java/com/banorte/prosperify/ProsperifyApplication.java
@@ -0,0 +1,13 @@
+package com.banorte.prosperify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsperifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProsperifyApplication.class, args);
+    }
+
+}
---
+++ backend/prosperify/src/test/java/com/banorte/prosperify/ProsperifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banorte.prosperify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsperifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
